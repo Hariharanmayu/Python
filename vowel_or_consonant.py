39b1643cd0b7fffbd742b4ca0429b0c69eebbbc4
Hariharanms <haranhari505@gmail.com>
2018-06-14 11:05:09 +0000

    Update vowel_or_consonant.py

--- vowel_or_consonant.py
+++ vowel_or_consonant.py
@@ -1,6 +1,9 @@
 i = input()
 ip = i.lower()
-if(ip == 'a' or ip == 'e' or ip == 'i' or ip == 'o' or ip == 'u'):
-    print("vowel")
+if(ip.isalpha()):
+    if(ip == 'a' or ip == 'e' or ip == 'i' or ip == 'o' or ip == 'u'):
+        print("vowel")
+    else:
+        print("consonant")
 else:
-    print("consonant")
+    print("invalid")
